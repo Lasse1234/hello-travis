1b3dfc12bd1237bb3fc30d15fea16b20e2d7b439
Lasse1234 <Cirkel@students.uni-marburg.de>
2017-09-05 15:53:08 +0200

    Aufgabe 2.1

--- src/main/java/Casting.java
+++ src/main/java/Casting.java
@@ -1,5 +1,25 @@
 
 public class Casting {
 
+	static void printChars() {
+	int start = 33;
+	while(start <127) {
+		char akkchar;
+		akkchar = (char) start;
+		//System.out.printf("Als Zahl: %d, als Char: %c\n", start, start);
+		System.out.println("Als int: " + start + ", als Char: " + akkchar);
+		start++;
+	}
+	}
 	
+	static int stringToInt(String number) {
+		int x = Integer.parseInt(number);
+		return x;
+	}
+	
+	public static void main(String[] args) {
+	printChars();
+	System.out.println(stringToInt("445"));
+		
+	}
 }
